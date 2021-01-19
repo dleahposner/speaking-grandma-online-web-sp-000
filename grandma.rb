37ad80a49919f1a32572581b829a24f75c1ca03d
Devorah Posner <dleahposner@gmail.com>
2021-01-19 22:20:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
        "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase
-     "HUH?! SPEAK UP, SONNY!"
-   else phrase.upcase
+   elsif phrase.upcase
      "NO, NOT SINCE 1938!"
+  # else
+  #   "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say 
